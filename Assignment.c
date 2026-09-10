03d7a0777fb0dcabcd251a9cfaa8c30cf83df696
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-10 20:31:19 +0200

    to be continue

---
+++ Assignment.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main () {
+    int num1;
+    int num2;
+    char operator;
+
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+
+     printf("Enter the first number:");
+    scanf("%c",&operator);
+
+     printf("Enter the first number:");
+    scanf("%d",&num2);
+
+    
+
+    
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ exercises.c
@@ -0,0 +1,23 @@
+
+# include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+   // printf("Hey bienfait!");
+int  number1, number2, sum;
+
+
+printf("enter number first number: ");
+scanf("%d", &number1);
+
+printf("enter number second number: ");
+scanf("%d", &number2);
+
+sum= number1 + number2;
+
+printf("The Total Sum is: %d",sum);
+
+
+
+    return 0;
+}

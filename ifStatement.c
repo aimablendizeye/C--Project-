c4b8b731295ff12d10f5f5eff0a5b825773a804c
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-10 17:48:25 +0200

    if statement

---
+++ ifStatement.c
@@ -0,0 +1,17 @@
+
+
+# include<stdio.h>
+
+int main () {
+
+     int age = 18;
+
+     if (age >= 18){
+        printf("You are allowed");
+     }
+     else {
+        printf("You are not allowed");
+     }
+
+    return 0;
+}
--- userInput.c
+++ userInput.c
@@ -3,11 +3,11 @@
 
 int main () {
     int age =0;
+    char name [] = 
     printf ("Enter You age: ");
     scanf("%d",&age);
 
     printf("%d",age);
-
 
 
     return 0;

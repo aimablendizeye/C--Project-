869320305ea9b1b86d2ea51c574d45122a3148e7
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-10 23:02:19 +0200

    Done assignment

--- Assignment.c
+++ Assignment.c
@@ -5,26 +5,49 @@ int main () {
     int num1;
     int num2;
     char operator;
+    int result ;
 
     printf("Enter the first number:");
     scanf("%d",&num1);
 
-     printf("Enter the first number:");
-    scanf("%c",&operator);
+     printf("Enter the second number:"); 
+     scanf("%d",&num2);
 
-     printf("Enter the first number:");
-    scanf("%d",&num2);
+     printf("Enter the operator number(+ or - or * or / or % ):\n");
+     scanf(" %c",&operator);
 
-    
+    if(operator == '+') {
+        result = num1+ num2;
+        printf("The result is: %d\n", result);
+    }
 
-    
+    else if (operator == '-'){
+        result = num1-num2;
 
+        printf("The result is: %d\n", result);
+    }
+      else if (operator == '*'){
+        result = num1*num2;
 
+        printf("The result is: %d\n", result);
+    }
 
+      else if (operator == '/'){
+        result = num1/num2;
 
+        printf("The result is: %d\n", result);
+    }
 
+      else if (operator == '%'){
+        result = num1 % num2;
 
-
-
+        printf("The result is: %d\n", result);
+    }
+    else {
+        printf("Invalid Operator!\n");
+        printf("pls choose one in the listed operators");
+        
+    }
+   
     return 0;
 }

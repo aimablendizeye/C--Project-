9121a35c8f45fc4764995ad5669a58e79fb4c9bd
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-11 13:06:55 +0200

    using switch case

--- Assignment.c
+++ Assignment.c
@@ -1,53 +1,80 @@
 
 #include <stdio.h>
+#include <math.h>
 
 int main () {
-    int num1;
-    int num2;
-    char operator;
-    int result ;
+    double num1 = 0.0;
+    double num2 = 0.0;
+    char operator = '\0';
+    double result = 0.0 ;
 
     printf("Enter the first number:");
-    scanf("%d",&num1);
+    scanf("%lf",&num1);
 
      printf("Enter the second number:"); 
-     scanf("%d",&num2);
+     scanf("%lf",&num2);
 
-     printf("Enter the operator number(+ or - or * or / or % ):\n");
-     scanf(" %c",&operator);
+     printf("Enter the operator number(+,-,*,/, % ):\n");
+     scanf(" %c", &operator);
 
-    if(operator == '+') {
-        result = num1+ num2;
-        printf("The result is: %d\n", result);
+         switch (operator) {
+        case '+':
+            result = num1+ num2;
+            break;
+        case '-':
+            result = num1- num2;
+            break;
+        case '*':
+            result = num1*num2;
+            break;   
+        case '/':
+            result = num1/ num2;
+            break;
+        case '%%':
+            result =fmod(num1,num2);
+            break;  
+            
+         default: 
+              printf("Invalid Operator!\n");
+              printf("pls choose one in the listed operators");
+                  
     }
 
-    else if (operator == '-'){
-        result = num1-num2;
-
-        printf("The result is: %d\n", result);
-    }
-      else if (operator == '*'){
-        result = num1*num2;
-
-        printf("The result is: %d\n", result);
-    }
-
-      else if (operator == '/'){
-        result = num1/num2;
-
-        printf("The result is: %d\n", result);
-    }
-
-      else if (operator == '%'){
-        result = num1 % num2;
-
-        printf("The result is: %d\n", result);
-    }
-    else {
-        printf("Invalid Operator!\n");
-        printf("pls choose one in the listed operators");
-        
-    }
+    printf("The result is : %.2f",result);
    
     return 0;
 }
+
+    // if(operator == '+') {
+    //     result = num1+ num2;
+    //     printf("The result is: %.2f\n", result);
+    // }
+
+    // else if (operator == '-'){
+    //     result = num1-num2;
+
+    //     printf("The result is: %.2f\n", result);
+    // }
+    //   else if (operator == '*'){
+    //     result = num1*num2;
+
+    //     printf("The result is: %.2f\n", result);
+    // }
+
+    //   else if (operator == '/'){
+    //     result = num1/num2;
+
+    //     printf("The result is:%.2f\n", result);
+    // }
+
+    //   else if (operator == '%'){
+    //     result = fmod(num1 , num2) ;
+
+    //     printf("The result is: %.2f\n", result);
+    // }
+    // else {
+    //     printf("Invalid Operator!\n");
+    //     printf("pls choose one in the listed operators");
+        
+    // }
+

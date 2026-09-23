349a41cc0513f701d33888f2ca86c81b3f936853
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-23 19:12:00 +0200

    improve Q2

--- assignment2_Q2.c
+++ assignment2_Q2.c
@@ -1,15 +1,16 @@
 
 #include<stdio.h>
+#include <math.h>
 
 
 int main (){
 
     float distance, consumption, fuel_price;
-    int tank_capacity;
+    double tank_capacity;
 
     float litre_needed,total_cost;
-    int fuel_tanks_required;
-    float litre_remaining;
+    int tanks_required;
+    int litre_remaining;
 
 
     printf("Enter the distance in (KM):");
@@ -21,6 +22,9 @@ int main (){
     printf("Enter Fuel Price (1L):");
     scanf("%f",&fuel_price);
 
+    printf ("Enter the tank capacity:");
+    scanf("%lf", &tank_capacity);
+
 
     if (distance <=0 || consumption <= 0 || fuel_price <=0)  {
         printf("\ninvalid input.\n");
@@ -29,10 +33,16 @@ int main (){
     }
     litre_needed = distance * consumption / 100;
     total_cost = fuel_price * litre_needed;
+    tanks_required = (int) (litre_needed / tank_capacity);
+    litre_remaining = fmod(litre_needed , tank_capacity);
+    
+
 
 
     printf ("Litres that needed:              %.2f\n",litre_needed);
-    printf("Total amount :                   %.2f",total_cost);
+    printf("Total amount :                   %.2f\n",total_cost);
+    printf("Total tank required:              %d\n",tanks_required);
+    printf("Fuel remaining is :              %d",litre_remaining);
 
 
 

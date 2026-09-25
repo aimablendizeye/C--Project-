46b6a90bba4e5295278f3d7e36faf6898d47752b
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-25 11:16:23 +0200

    learning function

--- function.c
+++ function.c
@@ -1,12 +1,47 @@
 
-#include<stdio.h> 
+#include<stdio.h>
+#include<string.h>
 
-// adding two numbers 
 
-void adding (int a,int b) {
-   
-    printf("%.2f", (float)a/b);
-} 
+
+
+//  Happy birthy Function 
+
+
+
+// void happy (int age, float price) {
+//     printf("Happy birthday you are %d , you should get a gift that cost: $%.2f" , age,price);
+//}
+
+
+// calculate function
+
+
+
+void calculate (int a, int b , char name [30]) {
+    printf("%d your name is : %s" , a + b, name);
+}
+
+
+
 int main () {
-   adding(34,5);
+
+    int a  = 0;
+    int b  = 0;
+    char name [30] = "";
+
+    printf("Enter the first number:");
+    scanf ("%d",&a);
+
+    printf("Enter the second number:");
+    scanf ("%d",&b);
+
+    printf("Enter your name :");
+    // fgets(name, sizeof(name), stdin);
+    // name[strlen(name) - 1] = '\0';
+    scanf ("%s",name);
+
+   calculate(a,b, name);
+
+    return 0;
 }
